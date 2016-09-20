0fd38416c8d8018aba43fdcc17b6ecb7b630362a
krishnanandnb <krishnanand@freshdesk.com>
2016-09-20 18:06:44 +0530

    refactoring code #7

--- BusinessHours.rb
+++ BusinessHours.rb
@@ -1,53 +1,53 @@
 require "time"
 class BusinessHours 
-	DAYS =[:Mon,:Tue,:Wed,:Thu,:Fri,:Sat,:Sun]
+	DAYS = [:Mon,:Tue,:Wed,:Thu,:Fri,:Sat,:Sun]
+	DATE_FORMAT = "%d-%m-%Y"
+	OUTPUT_DATE_FORMAT = "%a %b %d %H:%M:%S %Y"
 	def initialize(begin_time,end_time)
 		@week = Hash.new
 		working_hours = [begin_time,end_time]
 		DAYS.each{ |day | @week[day] = working_hours }
 		@hash_days=Hash[DAYS.map.with_index.to_a] 
 	end
+
 	def update(day,begin_time,end_time)
+		assign_timings(day,[begin_time,end_time])
+	end
+
+	def closed(day1,*days)
+		@week[day1] = [0,0]
+		days.each { |day| assign_timings(day,[0,0]) }
+	end
+
+	private
+
+	def assign_timings(day,begin_end)
 		if day.instance_of? Symbol
-			@week[day] = [begin_time,end_time]
+			@week[day] = begin_end
 		else
-			day=Time.parse(day).strftime("%d-%m-%Y")
-			@week[day]=[begin_time,end_time]
+			day=Time.parse(day).strftime(DATE_FORMAT)
+			@week[day]=begin_end
 
 		end
 	end
-	def closed(day1,*days)
-		@week[day1] = [0,0]
-		days.each do |day| 
-			if day.instance_of? Symbol
-				@week[day]=[0,0] 
-			else
-				day=Time.parse(day).strftime("%d-%m-%Y")
-				@week[day]=[0,0]
-			end
-		end
-	end
-	def checkForSameDay? (start_time_parsed,estimated_time)
-		start_time_parsed.strftime("%d-%m-%Y")==estimated_time.strftime("%d-%m-%Y")
-	end
+
 	def getClosingTime(estimated_time)
-		date=estimated_time.strftime("%d-%m-%Y")
-		return "#{date}+" "+#{@week[date][1]}" if @week[date]
-		day_of_theweek=estimated_time.strftime("%a").to_sym
-		"#{date}+" "+#{@week[day_of_theweek.to_sym][1]}"
+		getStartandEndTime(estimated_time,1)
 	end
+
 	def getStartTime(start_time)
-		date=start_time.strftime("%d-%m-%Y")
-		return "#{date}+" "+#{@week[date][0]}" if @week[date]
-		day_of_theweek=start_time.strftime("%a").to_sym
-		"#{date}+" "+#{@week[day_of_theweek.to_sym][0]}"
+		getStartandEndTime(start_time,0)
 	end
+
+	def getStartandEndTime(start_time,index)
+		date=start_time.strftime(DATE_FORMAT)
+		return "#{date}+" "+#{@week[date][index]}" if @week[date]
+		day_of_theweek=start_time.strftime("%a").to_sym
+		"#{date}+" "+#{@week[day_of_theweek.to_sym][index]}"
+	end
+
 	def checkforHoliday?(date)
-		if @week[date].nil? 
-			false 
-		 else
-		    @week[date][0]==0
-		end
+		 @week[date] ? @week[date][0]==0 : false
 	end
 		 
 			
@@ -58,32 +58,31 @@ class BusinessHours
 		begin
 		day_index=(day_index+1)%7	
 		new_date=(new_date+24*60*60)
-		end while @week[DAYS[day_index]][0]==0 || (checkforHoliday? new_date.strftime("%d-%m-%Y"))
+		end while @week[DAYS[day_index]][0]==0 || (checkforHoliday? new_date.strftime(DATE_FORMAT))
 		return @week[new_date][0] if @week[new_date]
-		return "#{new_date.strftime("%d-%m-%Y")} #{@week[DAYS[day_index]][0]}"
+		return "#{new_date.strftime(DATE_FORMAT)} #{@week[DAYS[day_index]][0]}"
 	end
+
 	def getDayIndex(day)
 		@hash_days[day]
 	end
+
+	public
+
 	def calculate_deadline(interval,start_time)
 		start_time_parsed=Time.parse start_time
 		start_time_of_day=Time.parse(getStartTime(Time.parse(start_time)))
 		estimated_time=start_time_of_day < start_time_parsed ? start_time_parsed+interval : start_time_of_day+interval
-		if checkForSameDay? start_time_parsed,estimated_time
-			closing_time =Time.parse(getClosingTime(estimated_time))
+		closing_time =Time.parse(getClosingTime(estimated_time))
 			if closing_time > estimated_time
-				#puts closing_time
-				#puts estimated_time
-				return estimated_time.strftime("%a %b %d %H:%M:%S %Y")
+				return estimated_time.strftime(OUTPUT_DATE_FORMAT)
 			else
 				remaining_time=estimated_time - closing_time
 				next_start_time=getNextDay(start_time_parsed)
-				#puts "next day start"+next_start_time
 				calculate_deadline(remaining_time,next_start_time)
 			end
-		else
 
-		end
+		
 	end
 	
 end
